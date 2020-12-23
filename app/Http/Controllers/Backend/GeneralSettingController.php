942a32d31803d64536c37dacf31438a0abb50ce6
phoeni2020 <you@example.com>
2020-12-23 16:48:40 +0200

    new update

---
+++ app/Http/Controllers/Backend/GeneralSettingController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Models\Generalsetting;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class GeneralSettingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Generalsetting  $generalsetting
+     * @return \Illuminate\Http\Response
+     */
+    public function show(GeneralSettingController $generalsetting)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Generalsetting  $generalsetting
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(GeneralSettingController $generalsetting)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Generalsetting  $generalsetting
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, GeneralSettingController $generalsetting)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Generalsetting  $generalsetting
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(GeneralSettingController $generalsetting)
+    {
+        //
+    }
+}
